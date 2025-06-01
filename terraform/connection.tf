d1550fe74071dd5381fc6dbd1feacae05b62ea36
greg-ogs <65621681+greg-ogs@users.noreply.github.com>
2025-05-31 18:09:29 -0600

    Remove redundant `sudo` usage in connection startup script

--- terraform/connection.tf
+++ terraform/connection.tf
@@ -67,13 +67,14 @@ resource "google_compute_instance" "bastion" {
   metadata_startup_script = <<-EOF
     #!/bin/bash
 
-    sudo apt-get update
-    sudo apt-get install apt-transport-https ca-certificates gnupg curl
+    apt-get update
+    apt-get install apt-transport-https ca-certificates gnupg curl
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    sudo apt-get update && sudo apt-get install google-cloud-cli
-    sudo apt-get install google-cloud-cli-gke-gcloud-auth-plugin
-    sudo apt-get install kubectl
+    apt-get update && sudo apt-get install google-cloud-cli
+    apt-get install google-cloud-cli-gke-gcloud-auth-plugin
+    apt-get install gke-gcloud-auth-plugin
+    apt-get install kubectl
 
   EOF
 
